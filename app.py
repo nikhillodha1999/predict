e6817de1e5ab140f231e064510400932f957bcb2
nikhillodha1999 <118475684+nikhillodha1999@users.noreply.github.com>
2023-04-11 21:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 
 from sklearn.linear_model import LogisticRegression
 
-model = LogisticRegression()
+model = LogisticRegression(max_iter=4000)
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
 
